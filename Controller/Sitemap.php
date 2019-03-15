58240da8c6f23b1b4f66505d97ab7196b0ae47bb
NeoRazorX <neorazorx@gmail.com>
2019-03-15 12:18:15 +0100

    Removed private teams from sitemap.

--- Controller/Sitemap.php
+++ Controller/Sitemap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Community plugin for FacturaScripts.
- * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -121,6 +121,10 @@ class Sitemap extends parentController
 
         $teamModel = new WebTeam();
         foreach ($teamModel->all([], ['lastmod' => 'DESC'], 0, 500) as $team) {
+            if ($team->private) {
+                continue;
+            }
+
             $items[] = $this->createItem($team->url('public'), strtotime($team->creationdate));
         }
 
