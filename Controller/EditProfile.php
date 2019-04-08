0bb0a9c3fa5cb1c926c178a89bcf795c4f6c129a
NeoRazorX <neorazorx@gmail.com>
2019-04-08 11:35:12 +0200

    Minor fix.

--- Controller/EditProfile.php
+++ Controller/EditProfile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of Community plugin for FacturaScripts.
- * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -54,8 +54,10 @@ class EditProfile extends parentController
         switch ($sectionName) {
             case 'ListWebTeamLog':
             case 'ListWebTeamMember':
-                $where[] = new DataBaseWhere('idcontacto', $this->contact->idcontacto);
-                $this->sections[$sectionName]->loadData('', $where);
+                if (!empty($this->contact) && $this->contact->exists()) {
+                    $where[] = new DataBaseWhere('idcontacto', $this->contact->idcontacto);
+                    $this->sections[$sectionName]->loadData('', $where);
+                }
                 break;
 
             default:
